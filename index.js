215e5f8da9f99eadb9707a3f3c5d0998d9c2237d
Your Name <you@example.com>
2020-06-04 13:39:37 -0400

    added env config

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ let express = require('express');
 const server = require("./api/server.js");
 let friendsRoutes = require('./friends/friendsRoutes');
 
+require("dotenv").config();
+
 server.use(express.json());
 server.use('/friends', friendsRoutes);
 
-server.get('/', (req, res) => {
-  res.status(200).json({api: "up"})
-})
+server.get("/", function (req, res) {
+  res.status(200).json({
+    environment: process.env.NODE_ENV,
+    port: process.env.PORT,
+    greeting: process.env.GREETING,
+  });
+});
 
 // const port = 5000;
-const port = process.env.PORT || 1123;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
