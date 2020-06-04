7adc922357e0f448ff6e9e5f18cfa35832d326c8
Your Name <you@example.com>
2020-06-04 12:51:33 -0400

    get resource added for /

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const server = require("./api/server.js");
 
+server.get('/', (req, res) => {
+  res.status(200).json({be: "up"})
+})
+
 // const port = 5000;
 const port = process.PORT || 1123;
 server.listen(port, () => {
